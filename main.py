0fb315b97da0f141392b0a0bd35009e8dbfa5858
Alexis Mainwaring <leximainw@gmail.com>
2022-08-17 12:53:24 -0400

    add hand scoring and dealer logic

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from deck import Deck
 class Game:
     def __init__(self, *players):
         self.dealer = Player()
+        self.curr_player = 0
         self.players = players
         self.deck = Deck.default()
         self.deck.shuffle()
@@ -14,6 +15,41 @@ class Game:
     def deal(self, player, cards=1):
         for _ in range(cards):
             player.add_card(self.deck.draw())
+
+    def play_next(self):
+        if self.curr_player == len(self.players):
+            self.play_dealer()
+            self.curr_player = 0
+        else:
+            self.play_player(self.players[self.curr_player])
+            self.curr_player += 1
+
+    def play_dealer(self):
+        if Game.score_hand(self.dealer.hand, True) < 17:
+            self.deal(self.dealer)
+
+    def play_player(self):
+        raise NotImplementedError()
+
+    def score_hand(hand, as_dealer=False):
+        score = 0
+        num_aces = 0
+        for card in hand:
+            if card.is_face():
+                score += 10
+            elif card.value != "Ace":
+                score += card.get_value()
+            else:
+                score += 11
+                num_aces += 1
+        if as_dealer:
+            score -= max(num_aces - 1, 0) * 10
+            num_aces = min(num_aces, 1)
+        for _ in range(num_aces):
+            if score <= 21:
+                break
+            score -= 10
+        return score
 
 
 class Player:
@@ -26,5 +62,3 @@ class Player:
 
 if __name__ == "__main__":
     game = Game(Player())
-    for card in game.players[0].hand.cards:
-        print(card)

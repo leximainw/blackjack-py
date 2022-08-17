4292ff11fe631c3c57629855e1b6ac9a42ac1818
Alexis Mainwaring <leximainw@gmail.com>
2022-08-17 11:58:37 -0400

    implement Card class

--- deck.py
+++ deck.py
@@ -5,6 +5,16 @@ VALUES = [
 ]
 
 
+class Card:
+    def __init__(self, face, value):
+        if face not in FACES:
+            raise (ValueError if type(face) is str else TypeError)()
+        if value not in VALUES:
+            raise (ValueError if type(value) is str else TypeError)()
+        self.face = face
+        self.value = value
+
+
 class Deck:
     def __init__(self):
         self.cards = []
@@ -13,5 +23,5 @@ class Deck:
         deck = Deck()
         for face in FACES:
             for value in VALUES:
-                deck.cards.append(f"{value} of {face}")
+                deck.cards.append(Card(face, value))
         return deck
